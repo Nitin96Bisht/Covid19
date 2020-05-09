3ae1f38094673f2be4378244afa3beb80afe116b
Nitin96Bisht <nitin.bisht96@gmail.com>
2020-05-09 22:47:54 +0530

    Added comma seperator for numbers

--- fetchData.js
+++ fetchData.js
@@ -4,10 +4,15 @@ fetch('https://api.covid19india.org/data.json')
 		appendData(data.statewise[0]);
 	})
 	.catch(err => console.error('Error: ', err));
+	
+	function numberWithCommas(x) {
+		return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	}
 		
 function appendData(data) {
 	var mainContainer = document.getElementById("covid19Data");
 	var div = document.createElement("div");
-	div.innerHTML = 'Confirmed: ' + data.confirmed + '<br><br>Active: ' + data.active + '<br><br>Recovered: ' + data.recovered + '<br><br>Deceased: ' + data.deaths;
+	div.innerHTML = 'Confirmed: ' + numberWithCommas(data.confirmed) + '<br><br>Active: ' + numberWithCommas(data.active) + '<br><br>Recovered: ' + 
+					numberWithCommas(data.recovered) + '<br><br>Deceased: ' + numberWithCommas(data.deaths);
 	mainContainer.appendChild(div);
 }
